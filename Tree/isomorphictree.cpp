daf7841aaab3e8b08d686ea550dddff06a77838b
anurag-singh2001 <anuragrazarwal@gmail.com>
2021-09-21 10:20:02 +0530

    Check if Tree is Isomorphic added

---
+++ Tree/isomorphictree.cpp
@@ -0,0 +1,19 @@
+bool isIsomorphic(Node *root1,Node *root2)
+    {
+    //add code here.
+    
+    if(root1==NULL && root2==NULL)
+        return true;
+     if(root1==NULL || root2==NULL)
+        return false;
+        
+    if(root1->data!=root2->data)
+        return false;
+    
+    
+    return
+ (isIsomorphic(root1->left,root2->left) && isIsomorphic(root1->right,root2->right))||
+ (isIsomorphic(root1->left,root2->right) && isIsomorphic(root1->right,root2->left));
+ 
+    
+    }
