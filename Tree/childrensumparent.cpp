0510f6e55415cf5e251893ca49e353ad7462bd11
anurag-singh2001 <anuragrazarwal@gmail.com>
2021-09-22 10:14:38 +0530

    Children Sum Parent added

---
+++ Tree/childrensumparent.cpp
@@ -0,0 +1,26 @@
+int isSumProperty(Node *root)
+{
+    // Add your code here
+    int left_data = 0, right_data = 0;
+
+    if (root == NULL ||
+        (root->left == NULL &&
+         root->right == NULL))
+        return 1;
+
+    else
+    {
+        if (root->left != NULL)
+            left_data = root->left->data;
+
+        if (root->right != NULL)
+            right_data = root->right->data;
+
+        if ((root->data == left_data + right_data) &&
+            isSumProperty(root->left) &&
+            isSumProperty(root->right))
+            return 1;
+        else
+            return 0;
+    }
+}
