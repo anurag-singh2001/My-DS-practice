616c1eec29f919bc5e3b8edd3e11c99aba5d025f
anurag-singh2001 <anuragrazarwal@gmail.com>
2021-10-27 10:20:35 +0530

    Insertion Sort added

---
+++ Array/insertion_sort.cpp
@@ -0,0 +1,15 @@
+void insertionSort(int arr[], int n)
+{
+    //code here
+    for (int i = 1; i < n; i++)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+}
