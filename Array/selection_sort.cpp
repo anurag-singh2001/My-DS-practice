0f5baa009e3023d3f55d3447641509e43617210b
anurag-singh2001 <anuragrazarwal@gmail.com>
2021-10-27 18:22:02 +0530

    Selection Sort added

---
+++ Array/selection_sort.cpp
@@ -0,0 +1,21 @@
+void selectionSort(int arr[], int n)
+{
+    //code here
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        if (min != i)
+        {
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+}
