898bbea9c22af33224c1c6e57f1957bfd25e4766
anurag-singh2001 <anuragrazarwal@gmail.com>
2022-06-03 13:12:04 +0530

    Replace every element with the least greater element on its right

---
+++ Tree/findLeastGreater.cpp
@@ -0,0 +1,14 @@
+vector<int> findLeastGreater(vector<int> &arr, int n)
+{
+    set<int> s;
+    vector<int> v(n, -1);
+    s.insert(arr[n - 1]);
+    for (int i = n - 2; i >= 0; i--)
+    {
+        auto it = s.upper_bound(arr[i]);
+        if (it != s.end())
+            v[i] = *it;
+        s.insert(arr[i]);
+    }
+    return v;
+}
