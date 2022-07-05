43f4b72ac2f8edacc90861bf1a83ec34b41e38ae
anurag-singh2001 <anuragrazarwal@gmail.com>
2022-07-05 18:18:24 +0530

    Maximum Rectangular Area in a Histogram

---
+++ Stack/reactanglearea.cpp
@@ -0,0 +1,29 @@
+long long getMaxArea(long long arr[], int n)
+{
+    // Your code here
+    stack<long long> s;
+    long long res = 0;
+    long long tp;
+    long long curr;
+
+    for (int i = 0; i < n; i++)
+    {
+        while (s.empty() == false && arr[s.top()] >= arr[i])
+        {
+            tp = s.top();
+            s.pop();
+            curr = arr[tp] * (s.empty() ? i : i - s.top() - 1);
+            res = max(res, curr);
+        }
+        s.push(i);
+    }
+    while (s.empty() == false)
+    {
+        tp = s.top();
+        s.pop();
+        curr = arr[tp] * (s.empty() ? n : n - s.top() - 1);
+        res = max(res, curr);
+    }
+
+    return res;
+}
