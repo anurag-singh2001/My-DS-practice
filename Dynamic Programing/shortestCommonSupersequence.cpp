12cc3d4ec2d3c6c62f05faf7fe3291c5379111ff
anurag-singh2001 <anuragrazarwal@gmail.com>
2022-08-10 17:17:27 +0530

    Shortest Common Supersequence

---
+++ Dynamic Programing/shortestCommonSupersequence.cpp
@@ -0,0 +1,19 @@
+int shortestCommonSupersequence(string X, string Y, int m, int n)
+{
+    // code here
+    int dp[m + 1][n + 1];
+    for (int i = 0; i < m + 1; i++)
+    {
+        for (int j = 0; j < n + 1; j++)
+        {
+            if (i == 0 or j == 0)
+                dp[i][j] = 0;
+            else if (X[i - 1] == Y[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    int x = dp[m][n];
+    return (m + n - x);
+}
