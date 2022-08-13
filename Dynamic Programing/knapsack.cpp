b9c838278bb891d50752aafb8a990a1f5b24bba5
anurag-singh2001 <anuragrazarwal@gmail.com>
2022-08-13 15:50:27 +0530

    0 - 1 Knapsack Problem

---
+++ Dynamic Programing/knapsack.cpp
@@ -0,0 +1,19 @@
+int knapSack(int W, int wt[], int val[], int n)
+{
+    // Your code here
+    int i, w;
+    vector<vector<int>> dp(n + 1, vector<int>(W + 1));
+    for (i = 0; i <= n; i++)
+    {
+        for (w = 0; w <= W; w++)
+        {
+            if (i == 0 or w == 0)
+                dp[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                dp[i][w] = max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
+            else
+                dp[i][w] = dp[i - 1][w];
+        }
+    }
+    return dp[n][W];
+}
