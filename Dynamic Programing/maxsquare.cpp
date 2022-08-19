7a4704ecb723f48404fa88513529bff973904bfc
anurag-singh2001 <anuragrazarwal@gmail.com>
2022-08-19 14:55:44 +0530

    Largest square formed in a matrix

---
+++ Dynamic Programing/maxsquare.cpp
@@ -0,0 +1,31 @@
+int solve(vector<vector<int>> &mat, int i, int j, int &maxi, vector<vector<int>> &dp)
+{
+
+    if (i >= mat.size() or j >= mat[0].size())
+        return 0;
+
+    if (dp[i][j] != -1)
+        return dp[i][j];
+
+    int right = solve(mat, i, j + 1, maxi, dp);
+    int diagnol = solve(mat, i + 1, j + 1, maxi, dp);
+    int down = solve(mat, i + 1, j, maxi, dp);
+
+    if (mat[i][j] == 1)
+    {
+        dp[i][j] = 1 + min(right, min(diagnol, down));
+        maxi = max(maxi, dp[i][j]);
+        return dp[i][j];
+    }
+    else
+        return dp[i][j] = 0;
+}
+
+int maxSquare(int n, int m, vector<vector<int>> mat)
+{
+    // code here
+    int maxi = 0;
+    vector<vector<int>> dp(n, vector<int>(m, -1));
+    solve(mat, 0, 0, maxi, dp);
+    return maxi;
+}
