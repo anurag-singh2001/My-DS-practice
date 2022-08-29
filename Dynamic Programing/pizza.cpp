3d3e6f436065c1ce19bb7c49d57ed7f036805ac6
anurag-singh2001 <anuragrazarwal@gmail.com>
2022-08-29 14:46:42 +0530

    Pizza With 3n Slices

---
+++ Dynamic Programing/pizza.cpp
@@ -0,0 +1,28 @@
+int solve(int index, int endindex, vector<int> &slices, int n, vector<vector<int>> &dp)
+{
+
+    if (n == 0 or index > endindex)
+        return 0;
+
+    if (dp[index][n] != -1)
+        return dp[index][n];
+
+    int take = slices[index] + solve(index + 2, endindex, slices, n - 1, dp);
+    int notTake = 0 + solve(index + 1, endindex, slices, n, dp);
+
+    return dp[index][n] = max(take, notTake);
+}
+
+int maxSizeSlices(vector<int> &slices)
+{
+
+    int n = slices.size();
+
+    vector<vector<int>> dp1(n, vector<int>(n, -1));
+    int case1 = solve(0, n - 2, slices, n / 3, dp1);
+
+    vector<vector<int>> dp2(n, vector<int>(n, -1));
+    int case2 = solve(1, n - 1, slices, n / 3, dp2);
+
+    return max(case1, case2);
+}
