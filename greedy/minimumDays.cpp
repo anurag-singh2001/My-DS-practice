3de12ee30dfa1b5a2e8763af3051e02532f00c60
anurag-singh2001 <anuragrazarwal@gmail.com>
2022-09-05 01:01:44 +0530

    Check if it is possible to survive on Island

---
+++ greedy/minimumDays.cpp
@@ -0,0 +1,18 @@
+int minimumDays(int S, int N, int M){
+        // code here
+        int sunday=S/7;
+        int buyingdays=S-sunday;
+        int totalfood=S*M;
+        int ans=0;
+        
+        if(totalfood%N==0)
+            ans=totalfood/N;
+        else
+            ans=totalfood/N+1;
+        
+        if(ans<=buyingdays)
+            return ans;
+        else
+            return -1;
+            
+    }
